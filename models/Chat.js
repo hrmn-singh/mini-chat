5a7581d79b0f2cb9ac1273f31fc3cc70226eb3aa
hrmn-singh <nagraharman08@gmail.com>
2025-12-23 10:29:14 +0530

    Create Chat model with participants and timestamp

---
+++ models/Chat.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const chatSchema = new mongoose.Schema({
+  participants: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Chat', chatSchema);
